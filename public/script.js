e69e7572b57b4c639de451742745b704c5d662f1
Derulo Taser <iih907@naver.com>
2021-03-22 14:02:43 +0900

    change host script

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
 const socket = io('/');
 const videoGrid = document.getElementById('video-grid');
 const myPeer = new Peer(undefined, {
-  host: '/',
+  host: 'www.shadow-fit.com',
   secure: true,
   port: '443',
 });
